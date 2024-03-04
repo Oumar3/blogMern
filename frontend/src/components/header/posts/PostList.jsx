cdc9982fb5995863173eb2ec1c125fb2c75bcf5f
OUMAR3 <oumaralitori1@gmail.com>
2024-03-04 12:25:29 +0100

    c'est deja cool post et postItem components

---
+++ frontend/src/components/header/posts/PostList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './post.css'
+import PostItem from './PostItem'
+
+const PostList = ({ posts }) => {
+    return (
+        <div className='post-list'>
+            {
+                posts.map(post => <PostItem post={post} key={post._id} />)
+            }
+        </div>
+    );
+}
+
+export default PostList;
